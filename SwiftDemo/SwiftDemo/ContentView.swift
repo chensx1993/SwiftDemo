8624ff8559dd95a5a6ee6363cfdd6e3a8e628668
chensx <mia.s.x.chen@qq.com>
2020-06-16 15:19:25 +0800

    feat: add new project

---
+++ SwiftDemo/SwiftDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by dingjianjaja on 2020/6/16.
+//  Copyright © 2020 chensx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
